0bd62a7814e3d095106616baf766cd78f7ba76fe
Angie <43548929+Angie1210@users.noreply.github.com>
2022-05-21 12:16:28 -0700

    geojson3

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working geojson");
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // An array containing each city's location, state, and population.
 
@@ -12,27 +12,30 @@ let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Loop through the cities array and create one marker for each city.
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+//L.geoJSON(sanFranAirport).addTo(map);
+// Grabbing our GeoJSON data.
+/* L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+      .bindPopup("<h2>" + feature.properties.city + "</h2><hr> <h3>"+ feature.properties.faa+"</h3> ")
+
+    }
+
+  }).addTo(map); */
+
+L.geoJSON(sanFranAirport,{
+
+    onEachFeature: function(feature,layer){
+        console.log(layer);
+        layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+
+    }
+}).addTo(map);
 
 
  /*   L.circlemarker(city.location,{
@@ -41,10 +44,14 @@ L.geoJSON(sanFranAirport).addTo(map);
 
 //L.marker(city.location)
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/light-v10/mapbox//tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData ="https://raw.githubusercontent.com/Angie1210/Mapping_Earthquakes/main/majorAirports.json";
+

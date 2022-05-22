dabcd521634e1ee5b71a915d3b4a0087237ed702
Angie <43548929+Angie1210@users.noreply.github.com>
2022-05-22 12:18:59 -0700

    earth9

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -73,7 +73,7 @@ function getColor(magnitude) {
     }
     return "#98ee00";
   }
-  
+
 
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
@@ -89,12 +89,18 @@ function getRadius(magnitude) {
 d3.json(earthqueakes).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data,{
-    
+    // We turn each feature into a circleMarker on the map.
 pointToLayer: function(feature, latlng) {
         console.log(data)    
         return L.circleMarker(latlng);
 
   },
-  style: styleInfo
- } ).addTo(map);
+  // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+ } 
+}).addTo(map);
 });

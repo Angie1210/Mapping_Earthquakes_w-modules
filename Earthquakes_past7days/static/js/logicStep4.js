226c2571184dda0dc2ba076e7cea725458990f97
Angie <43548929+Angie1210@users.noreply.github.com>
2022-05-22 12:40:46 -0700

    overlayer1

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -104,21 +104,21 @@ d3.json(earthqueakes).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data,{
     // We turn each feature into a circleMarker on the map.
-pointToLayer: function(feature, latlng) {
+    pointToLayer: function(feature, latlng) {
         console.log(data)    
         return L.circleMarker(latlng);
 
-  },
+    },
   // We set the style for each circleMarker using our styleInfo function.
   style: styleInfo,
     // We create a popup for each circleMarker to display the magnitude and
     //  location of the earthquake after the marker has been created and styled.
     onEachFeature: function(feature, layer) {
         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
- } 
-}).addTo(earthquakes);
+    } 
+    }).addTo(earthquakes);
 
 //then we add the earthquae layer to our map
-earthquakes.addTo(map);
+    earthquakes.addTo(map);
 
 });
